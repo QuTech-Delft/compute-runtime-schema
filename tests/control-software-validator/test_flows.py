373c4b5053cd8f2b9558160b353a95ae8d848e84
Mythir <larsvanleeuwen94@gmail.com>
2025-03-19 16:44:27 +0100

    Add two init flow

--- tests/control-software-validator/test_flows.py
+++ tests/control-software-validator/test_flows.py
@@ -5,6 +5,10 @@ import pytest
 import zmq
 from models.execute_reply_success import ExecuteReplySuccess
 from models.execute_request import ExecuteRequest, QuantumHardwareRunCircuitPayload
+from models.get_dynamic_reply_success import GetDynamicReplySuccess
+from models.get_dynamic_request import GetDynamicRequest
+from models.get_static_reply_success import GetStaticReplySuccess
+from models.get_static_request import GetStaticRequest
 from models.initialize_reply_success import InitializeReplySuccess
 from models.initialize_request import InitializeRequest
 from models.publish_state_message import PublishStateMessage
@@ -41,7 +45,24 @@ async def test_publish_state(sub_stream: Socket) -> None:
     _ = PublishStateMessage.model_validate_json(message)
 
 
+async def test_static_data_request(req_stream: Socket) -> None:
+    # Test if static data reply is correctly formatted
+    static_data_request = GetStaticRequest(version=version, command="get_static")
+    req_stream.send_string(static_data_request.model_dump_json())
+    message = await req_stream.recv_string()
+    _ = GetStaticReplySuccess.model_validate_json(message)
+
+
+async def test_dynamic_data_request(req_stream: Socket) -> None:
+    # Test if dynamic data reply is correctly formatted
+    dynamic_data_request = GetDynamicRequest(version=version, command="get_dynamic")
+    req_stream.send_string(dynamic_data_request.model_dump_json())
+    message = await req_stream.recv_string()
+    _ = GetDynamicReplySuccess.model_validate_json(message)
+
+
 async def test_happy_flow(req_stream: Socket):
+    # Test normal init->execute->terminate flow
     session_id = uuid.uuid4()
 
     init_request = InitializeRequest(
@@ -69,6 +90,53 @@ async def test_happy_flow(req_stream: Socket):
     message = await req_stream.recv_string()
 
     _ = ExecuteReplySuccess.model_validate_json(message)
+
+    terminate_request = TerminateRequest(
+        version=version, session_id=session_id, command="terminate"
+    )
+    req_stream.send_string(terminate_request.model_dump_json())
+    message = await req_stream.recv_string()
+
+    _ = TerminateReplySuccess.model_validate_json(message)
+
+
+async def test_two_init_flow(req_stream: Socket):
+    # Test if two init requests are rejected
+    session_id = uuid.uuid4()
+
+    init_request = InitializeRequest(
+        version=version, session_id=session_id, command="initialize"
+    )
+    req_stream.send_string(init_request.model_dump_json())
+    message = await req_stream.recv_string()
+
+    _ = InitializeReplySuccess.model_validate_json(message)
+
+    exec_payload = QuantumHardwareRunCircuitPayload(
+        job_id=1,
+        circuit="version 1.0\nqubit[1] q\nbit[1] b\nX q[0]\nb[0] = measure q[0]",
+        number_of_shots=10,
+        include_raw_data=False,
+    )
+    exec_request = ExecuteRequest(
+        version=version,
+        session_id=session_id,
+        command="execute",
+        payload=exec_payload,
+    )
+
+    req_stream.send_string(exec_request.model_dump_json())
+    message = await req_stream.recv_string()
+
+    _ = ExecuteReplySuccess.model_validate_json(message)
+
+    init_request = InitializeRequest(
+        version=version, session_id=session_id, command="initialize"
+    )
+    req_stream.send_string(init_request.model_dump_json())
+    message = await req_stream.recv_string()
+
+    _ = InitializeReplySuccess.model_validate_json(message)
 
     terminate_request = TerminateRequest(
         version=version, session_id=session_id, command="terminate"
