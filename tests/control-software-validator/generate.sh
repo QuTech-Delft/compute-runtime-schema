e6752d96f07101919ac84116761276be15d7a01f
Mythir <larsvanleeuwen94@gmail.com>
2025-03-19 15:31:04 +0100

    Remove useless comments

--- tests/control-software-validator/generate.sh
+++ tests/control-software-validator/generate.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-#
+
 # Directory to search
 schemas="../../schemas"
 models="./models"
 
-# Loop through all files in the directory and subdirectories
+# Loop through all files in the schemas directory and subdirectories
 find "$schemas" -type f -name "*.json" | while read -r file; do
-    # Extract file name without extension
     filename=$(basename "$file" | sed 's/\..*//')
-    # Extract first parent directory
     parentdir=$(basename "$(dirname "$file")")
     classname="${parentdir}_${filename}"
+
     datamodel-codegen \
         --input "$file" \
         --class-name $classname \
