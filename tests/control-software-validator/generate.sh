bd2260138d601092ed6e56d4aa148b82abc8ac3e
Mythir <larsvanleeuwen94@gmail.com>
2025-03-19 15:19:35 +0100

    Add model generation

---
+++ tests/control-software-validator/generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Directory to search
+schemas="../../schemas"
+models="./models"
+
+# Loop through all files in the directory and subdirectories
+find "$schemas" -type f -name "*.json" | while read -r file; do
+    # Extract file name without extension
+    filename=$(basename "$file" | sed 's/\..*//')
+    # Extract first parent directory
+    parentdir=$(basename "$(dirname "$file")")
+    classname="${parentdir}_${filename}"
+    datamodel-codegen --input "$file" --class-name $classname --output "./models/$classname.py" --output-model-type pydantic_v2.BaseModel
+done
