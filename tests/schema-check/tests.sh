923fb968755e3ef3f7bc279573297496291330f9
Mythir <larsvanleeuwen94@gmail.com>
2025-06-19 10:46:50 +0200

    Update test script

--- tests/schema-check/tests.sh
+++ tests/schema-check/tests.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
+let max_version=1
+while [ -d ../../schemas/v$((max_version+1)) ]; do
+    max_version=$((max_version + 1))
+done
+
 echo Test initialize
-check-jsonschema --schemafile ../../schemas/initialize/request.schema.json ./data/initialize/request.json
-check-jsonschema --schemafile ../../schemas/initialize/reply.schema.json ./data/initialize/reply.success.json ./data/initialize/reply.failure.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareInitializeRequest.schema.json ./data/initialize/request.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareSimpleSuccessResponse.schema.json ./data/initialize/reply.success.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareFailureResponse.schema.json ./data/initialize/reply.failure.json
 
 echo Test terminate
-check-jsonschema --schemafile ../../schemas/terminate/request.schema.json ./data/terminate/request.json
-check-jsonschema --schemafile ../../schemas/terminate/reply.schema.json ./data/terminate/reply.success.json ./data/initialize/reply.failure.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareTerminateRequest.schema.json ./data/terminate/request.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareSimpleSuccessResponse.schema.json ./data/terminate/reply.success.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareFailureResponse.schema.json ./data/terminate/reply.failure.json
 
 echo Test execute
-check-jsonschema --schemafile ../../schemas/execute/request.schema.json ./data/execute/request.json
-check-jsonschema --schemafile ../../schemas/execute/reply.schema.json ./data/execute/reply.success.json ./data/execute/reply.failure.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareExecuteRequest.schema.json ./data/execute/request.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareExecuteResponse.schema.json ./data/execute/reply.success.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareFailureResponse.schema.json ./data/execute/reply.failure.json
 
 echo Test get_static
-check-jsonschema --schemafile ../../schemas/get_static/request.schema.json ./data/get_static/request.json
-check-jsonschema --schemafile ../../schemas/get_static/reply.schema.json ./data/get_static/reply.success.json ./data/get_static/reply.failure.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareStaticDataRequest.schema.json ./data/get_static/request.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareStaticDataResponse.schema.json ./data/get_static/reply.success.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareFailureResponse.schema.json ./data/get_static/reply.failure.json
 
 echo Test get_dynamic
-check-jsonschema --schemafile ../../schemas/get_dynamic/request.schema.json ./data/get_dynamic/request.json
-check-jsonschema --schemafile ../../schemas/get_dynamic/reply.schema.json ./data/get_dynamic/reply.success.json ./data/get_dynamic/reply.failure.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareDynamicDataRequest.schema.json ./data/get_dynamic/request.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareDynamicDataResponse.schema.json ./data/get_dynamic/reply.success.json
+check-jsonschema --schemafile ../../schemas/$max_version/QuantumHardwareFailureResponse.schema.json ./data/get_dynamic/reply.failure.json
 
 echo Test publish_state
-check-jsonschema --schemafile ../../schemas/publish_state/message.schema.json ./data/publish_state/message.json
+check-jsonschema --schemafile ../../schemas/max_version/PublishState.schema.json ./data/publish_state/message.json
